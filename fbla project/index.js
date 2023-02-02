db798e246684691677ff4f7c81deaf2b1584f365
jaiom <jaiomrola@gmail.com>
2023-02-02 15:40:13 -0600

    added position in maps

--- fbla project/index.js
+++ fbla project/index.js
@@ -1,10 +1,29 @@
 let map;
 
 function initMap() {
+  const the_living_room = { lat: 30.266473, lng: -97.744242 }
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 30.5083, lng: -97.6789 },
-    zoom: 8,
+    center: the_living_room,
+    zoom: 15,
+  });
+  const marker = new google.maps.Marker({
+    position: the_living_room,
+    map,
+    title: "The Living Room",
+  });
+
+  const content_string = 'The Living Room'
+  const infowindow = new google.maps.InfoWindow({
+    content: content_string,
+  });
+ 
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+    });
   });
 }
+
 
 window.initMap = initMap;
