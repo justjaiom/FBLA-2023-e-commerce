1c30a4bc97c6d7eff66db7c07877b306ae3511bc
jaiom <jaiomrola@gmail.com>
2023-02-02 20:23:01 -0600

    added slideshow

--- fbla project/index.js
+++ fbla project/index.js
@@ -1,7 +1,7 @@
 let map;
 
 function initMap() {
-  const the_living_room = { lat: 30.266473, lng: -97.744242 }
+  const the_living_room = { lat: 30.266473, lng: -97.744242 };
   map = new google.maps.Map(document.getElementById("map"), {
     center: the_living_room,
     zoom: 15,
@@ -12,18 +12,18 @@ function initMap() {
     title: "The Living Room",
   });
 
-  const content_string = 
-  '<div id="content">' +
+  const content_string =
+    '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
     '<h1 id="firstHeading" class="firstHeading">The Living Room</h1>' +
-    '<div id="bodyContent">' + 
-    "<p> 120 W 4th St Austin, TX 78701 </p>"
+    '<div id="bodyContent">' +
+    "<p> 120 W 4th St Austin, TX 78701 </p>";
 
   const infowindow = new google.maps.InfoWindow({
     content: content_string,
   });
- 
+
   marker.addListener("click", () => {
     infowindow.open({
       anchor: marker,
@@ -31,6 +31,5 @@ function initMap() {
     });
   });
 }
-
 
 window.initMap = initMap;
