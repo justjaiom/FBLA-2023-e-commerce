97df9292b75849a4657e88b79adafac7894be882
jaiom <jaiomrola@gmail.com>
2023-03-04 16:39:39 -0600

    nearby locations

--- fbla project/index.js
+++ fbla project/index.js
@@ -1,10 +1,14 @@
-let map;
 function initMap() {
+  // Create the map.
   const the_living_room = { lat: 29.282981, lng: -94.800104 };
-  map = new google.maps.Map(document.getElementById("map"), {
+  const map = new google.maps.Map(document.getElementById("map"), {
     center: the_living_room,
-    zoom: 15,
+    zoom: 17,
+    mapId: "8d193001f940fde3",
+
   });
+
+  //create marker
   const marker = new google.maps.Marker({
     position: the_living_room,
     map,
@@ -29,7 +33,65 @@ function initMap() {
       map,
     });
   });
+  // Create the places service.
+  const service = new google.maps.places.PlacesService(map);
+  let getNextPage;
+  const moreButton = document.getElementById("more");
+
+  moreButton.onclick = function () {
+    moreButton.disabled = true;
+    if (getNextPage) {
+      getNextPage();
+    }
+  };
+
+  // Perform a nearby search.
+  service.nearbySearch(
+    { location: the_living_room, radius: 2500, type: "store" },
+    (results, status, pagination) => {
+      if (status !== "OK" || !results) return;
+
+      addPlaces(results, map);
+      moreButton.disabled = !pagination || !pagination.hasNextPage;
+      if (pagination && pagination.hasNextPage) {
+        getNextPage = () => {
+          // Note: nextPage will call the same handler function as the initial call
+          pagination.nextPage();
+        };
+      }
+    }
+  );
+}
+
+function addPlaces(places, map) {
+  const placesList = document.getElementById("places");
+
+  for (const place of places) {
+    if (place.geometry && place.geometry.location) {
+      const image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      new google.maps.Marker({
+        map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location,
+      });
+
+      const li = document.createElement("li");
+
+      li.textContent = place.name;
+      placesList.appendChild(li);
+      li.addEventListener("click", () => {
+        map.setCenter(place.geometry.location);
+      });
+    }
+  }
 }
 
 window.initMap = initMap;
-
